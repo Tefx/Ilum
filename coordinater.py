b8ace4a2ec301ade8ac3ff0186d7569249c58066
Zhu Zhaomeng <Zhaomeng.Zhu@me.com>
2012-04-17 13:37:34 +0800

    It works!

--- coordinater.py
+++ coordinater.py
@@ -1,17 +1,17 @@
 import gevent
-from gevent.socket import socket, AF_INET, SOCK_DGRAM
+from gevent.socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
 from cPickle import dumps, loads
+from sys import argv
+import worker
 
 
 class Coordinater(object):
-	def __init__(self):
+	def __init__(self, port):
 		self.workers = []	
 		self.sock = socket(AF_INET, SOCK_DGRAM)
-		self.sock.bind(('', 8523))
-		self.release = self.add_worker
+		self.sock.bind(('', port))
 
 	def add_worker(self, worker):
-		print "New worker: ", worker
 		self.workers.append(worker)
 
 	def require(self, num):
@@ -20,13 +20,13 @@ class Coordinater(object):
 		while got < num and self.workers:
 			res.append(self.workers.pop(0))
 			got += 1
-		print got, "workers going:", res
-		return (got, res)
+		return res
 
 	def run(self):
 		while True:
 			data, addr = self.sock.recvfrom(1024)
-			gevent.spawn(self.handle, loads(data), addr)
+			#gevent.spawn(self.handle, loads(data), addr)
+			self.handle(loads(data), addr)
 
 	def handle(self, data, addr):
 		if data[0] == "ADD":
@@ -35,5 +35,37 @@ class Coordinater(object):
 			res = self.require(data[1])
 			self.sock.sendto(dumps(res), addr)
 
+class CoordinaterClient(object):
+	def __init__(self, coord_addr):
+		self.coord_addr = coord_addr
+		self.coon = socket(AF_INET, SOCK_DGRAM)
+
+	def add(self, addr):
+		self.coon.sendto(
+			dumps(["ADD", addr]), 
+			self.coord_addr
+			)
+
+	def make_worker(self, addr):
+		sock = socket(AF_INET, SOCK_STREAM)
+		if sock.connect_ex(addr) != 0:
+			return None
+		else:
+			return worker.WorkerClient(sock)
+
+	def require(self, num):
+		self.coon.sendto(
+			dumps(["REQ", num]), 
+			self.coord_addr
+			)
+		data, addr = self.coon.recvfrom(40960000)
+		return [worker for worker in [self.make_worker(addr) for addr in loads(data)] if worker]
+
 if __name__ == "__main__":
-	Coordinater().run()
+	if len(argv) < 2:
+		port = 8523
+	else:
+		port = int(argv[1])
+
+	Coordinater(port).run()
+
--- worker.py
+++ worker.py
@@ -2,78 +2,79 @@ import gevent
 from gevent.socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM, gethostname, gethostbyname
 from cPickle import dumps, loads
 from sys import argv
+import coordinater
+from funcs import *
 
 class Worker(object):
 
 	def __init__(self, local_addr, coord_addr):
-		self.local_ip, self.port = local_addr
-		self.coord_addr = coord_addr
-
+		self.coord = coordinater.CoordinaterClient(coord_addr)
+		self.local_addr = local_addr
 		self.sock = socket(AF_INET, SOCK_STREAM) 
-		self.sock.bind(("", self.port))  
+		self.sock.bind(("", self.local_addr[1])) 
 		self.sock.listen(100) 
-
-
-	def cry_when_born(self):
-		udpSock = socket(AF_INET, SOCK_DGRAM)
-		udpSock.sendto(dumps(["ADD", (self.local_ip, self.port)]), self.coord_addr)
-
+		self.coord.add(self.local_addr)
+		
 	def run(self):
 		while True:
 			conn, address = self.sock.accept()  
-        	fileobj = conn.makefile()
-        	line = fileobj.readline()
-        	if line:
-        		result = self.eval(loads(line.strip()))
-        		fileobj.write(dumps(result)+"\r\n")
-        		fileobj.flush()
-        	conn.close()
+			data = loads(conn.recv(40960000))
+			res = self.eval(data)
+			conn.sendall(dumps(res))
+			self.coord.add(self.local_addr)
+			conn.close()
 
-	def eval(self, E):
-		if not isinstance(E, list):
-			return E
+	def eval(self, e, local=False):
+		if not isinstance(e, list): return e
+		if e[0] == "local": return self.eval(e[1:], True)
+
+		if local:
+			return self.apply(e[0], [self.eval(item, True) for item in e[1:]], True)
 		else:
-			return self.apply(E[0], self.map_eval(E[1:]))
+			return self.apply(e[0], self.map_eval(e[1:]))
 
-	def apply(self, func, args):
+	def apply(self, func, args, local=False):
+		if local: return func(*args)
+
+		if func == map:
+			el = [[args[0], arg] for arg in args[1]]
+			return self.map_eval(el)
+		elif func == "dot":
+			return dot(*args)
 		return func(*args)
+
+	def split_jobs(self, jobs, workers):
+		num_each = len(jobs) / len(workers)
+		if num_each * len(workers) < len(jobs):
+			num_each += 1
+		res = []
 
 	def map_eval(self, L):
 		if not L: return []
-
 		num_job = len(L)
 		if num_job == 1: 
 			return [self.eval(L[0])]
 
-		num_worker, workers = self.require_workers(num_job)
+		workers = self.coord.require(num_job)
+		num_worker = len(workers)
 		if num_worker == 0:
 			result_first = self.eval(L.pop())
 			return [result_first] + self.map_eval(L)
 		else:
 			jobs = [gevent.spawn(worker.eval, job) for (worker, job) in zip(workers, L[:num_worker])]
 			gevent.joinall(jobs)
-			return [job.value for job in jobs].extend(self.map_eval(L[num_worker:]))
-
-	def require_workers(self, Num):
-		udpSock = socket(AF_INET, SOCK_DGRAM)
-		udpSock.sendto(dumps(["REQ", Num]), self.coord_addr)
-		data, addr = udpSock.recvfrom(40960000)
-		num, worker_addr = data
-		return num, [WorkerClient(addr) for addr in worker_addr]
-
+			return [job.value for job in jobs] + self.map_eval(L[num_worker:])
 
 class WorkerClient(object):
-	def __init__(self, address):
-		self.sock = socket(AF_INET, SOCK_STREAM)
-		self.sock.connect(address)
-		self.fileobj = socket.makefile()
+	def __init__(self, sock):
+		self.sock = sock
+
 
 	def eval(self, E):
-		self.fileobj.write(dumps(E)+"\r\n")
-		self.fileobj.flush()
-		result = self.fileobj.readline()
+		self.sock.sendall(dumps(E))
+		result = self.sock.recv(40960000)
 		self.sock.close()
-		return loads(result.strip())
+		return loads(result)
 
 if __name__ == '__main__':
 	local_ip, local_port_str = argv[1].split(':')
@@ -82,7 +83,6 @@ if __name__ == '__main__':
 	coord_addr = (coord_ip, int(coord_port_str))
 
 	worker = Worker(local_addr, coord_addr)
-	worker.cry_when_born()
 	worker.run()
 
 
